eed2fd29d7ce913e4ac1b1ccf6aae660f4a2f41a
dimasfarrabi <dimas.farrabi@gmail.com>
2023-07-23 00:55:44 +0700

    7th commit

--- check-in-receiver/server.js
+++ check-in-receiver/server.js
@@ -11,7 +11,7 @@ var corsOptions = {
 };
 
 async function consumeMessages() {
-  const connection = await amqp.connect("amqp://localhost:5673");
+  const connection = await amqp.connect("amqp://guest:guest@rabbitmq:5672");
   const channel = await connection.createChannel();
 
   await channel.assertExchange("logExchange", "direct");
