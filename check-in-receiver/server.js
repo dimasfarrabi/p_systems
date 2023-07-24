f9a4299cce26ee8e30e1eeece0210c088a5d0d90
dimasfarrabi <dimas.farrabi@gmail.com>
2023-07-24 22:26:47 +0700

    10th commit

--- check-in-receiver/server.js
+++ check-in-receiver/server.js
@@ -25,13 +25,13 @@ async function consumeMessages() {
     var sql = "INSERT INTO checkin_transactions(park_id, vehicle_id, unique_id, is_checkout, createdAt, updatedAt) VALUES ('"+data.location+"',(SELECT id FROM vehicle_types WHERE vehicle = '"+data.type+"' LIMIT 1),'"+data.specialid+"','0','"+data.dateTime+"',NOW())";
     console.log(sql);
     let connection = mysqli.createConnection(configure);
-    /*connection.query(sql, (error, results) => {
+    connection.query(sql, (error, results) => {
         if (error){
           console.log(error.message);
         }
         console.log('Rows affected:', results.affectedRows);
     });
-    connection.end();*/
+    connection.end();
     channel.ack(msg);
   });
 }
